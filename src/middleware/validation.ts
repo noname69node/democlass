40311956113b692abc52056bdb3aa7561c9d51ef
Paweł <nonamenodejs@gmail.com>
2023-09-08 17:55:06 +0300

    UserCreateDto trying

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -1,17 +1,21 @@
 import { plainToClass } from "class-transformer";
 import { validate, ValidationError } from "class-validator";
 import * as express from "express";
+import { sanitize } from "class-sanitizer";
 //import HttpException from '../exceptions/HttpException';
 
 function validationMiddleware<T>(type: any): express.RequestHandler {
   return (req, res, next) => {
-    validate(plainToClass(type, req.body)).then((errors: ValidationError[]) => {
+    const dtoObj = plainToClass(type, req.body);
+    validate(dtoObj).then((errors: ValidationError[]) => {
       if (errors.length > 0) {
         const message = errors;
         //   .map((error: ValidationError) => Object.values(error.constraints))
         //   .join(", ");
         next(new Error(message.toString()));
       } else {
+        sanitize(dtoObj);
+        req.body = dtoObj;
         next();
       }
     });
